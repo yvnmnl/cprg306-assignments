47ec5b89ff72887da11e1bca29de15b235364949
yvnmnl <yvana.gaddao@edu.sait.ca>
2025-09-18 20:40:41 -0600

    created week-2 page, StudentInfo component, rendered StudentInfo into week-2 page

---
+++ app/week-2/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentInfo from './student-info';
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+
+      {/* Student Info Component */
+        <StudentInfo />}
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function StudentInfo() {
+  return (
+    <div>
+      <h1>Yvana Gaddao</h1>
+      <p>
+        <a href="https://github.com/yvnmnl/cprg306-assignments" target="_blank" rel="noopener noreferrer">My GitHub Repository</a>
+      </p>
+    </div>
+  );
+}
+
+export default StudentInfo;
