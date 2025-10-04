b7d17687fcf95e09c07281ab89191fc80cdf8f24
yvnmnl <yvana.gaddao@edu.sait.ca>
2025-10-03 23:05:28 -0600

    added styling
    added button styling like colour and disabled features using tailwind

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -16,14 +16,15 @@ export default function NewItem() {
       setQuantity(quantity + 1);
     }
   };
-
+ 
   return (
-    <div>
-      <p>Quantity: {quantity}</p>
-      <div>
-        <button onClick={decrement} disabled={quantity === 1}> - </button>
-        <button onClick={increment} disabled={quantity === 20}> + </button>
+    <div className="mb-2 p-2 border rounded shadow-sm text-black text-center bg-gray-200/50">
+      <p className="mb-2 font-semibold">Quantity: <span style={{color: '#373d20'}}>{quantity}</span></p>
+      <div className="flex space-x-2 justify-center">
+        <button onClick={decrement} disabled={quantity === 1} className="px-3 py-1 border rounded disabled:opacity-50 disabled:cursor-not-allowed bg-[#D4BECF]"> - </button>
+        <button onClick={increment} disabled={quantity === 20} className="px-3 py-1 border rounded disabled:opacity-50 disabled:cursor-not-allowed bg-[#BED4C3]"> + </button>
       </div>
+      <p className="text-xs">Range must be within 1 - 20</p>
     </div>
   );
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -5,7 +5,7 @@ function Page() {
   return (
     <main className="flex justify-center min-h-screen py-2 items-start bg-gradient-to-b from-[#035718] via-white to-[#FC5A67]"> 
       <div className="w-full max-w-md flex flex-col">
-        <h1 className="text-2xl font-bold mb-3 text-gray-100">Add New Item</h1>
+        <h1 className="text-2xl font-bold mb-3">Add New Item</h1>
         <NewItem /> 
       </div>
     </main>
