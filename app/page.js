f20f30043851d0ef31053e94dde1d9a397836cb4
yvnmnl <yvana.gaddao@edu.sait.ca>
2025-10-14 19:44:46 -0600

    Week-5 set up
    Added in week-5 folder and link on app/page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,8 @@ export default function HomePage() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
 
       <p>
-        <Link href="/week-2">Go to Week 2 →</Link> <Link href="/week-3">Go to Week 3 →</Link> <Link href="/week-4">Go to Week 4 →</Link> 
+        <Link href="/week-2">Go to Week 2 →</Link> <Link href="/week-3">Go to Week 3 →</Link> <Link href="/week-4">Go to Week 4 →</Link>
+        <Link href="/week-5">Go to Week 5 →</Link>  
       </p>
     </main>
   );
