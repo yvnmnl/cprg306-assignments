0905fa567393fa59905c154494e776360b722ab6
yvnmnl <yvana.gaddao@edu.sait.ca>
2025-11-24 20:13:19 -0700

    Added getItems Function
    Updated the shopping-list-service.js to have the getItem function

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -1,2 +1,19 @@
 import { db } from "../_utils/firebase";
 import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+export async function getItems(userId) {
+  if (!userId) return [];
+
+  const itemsRef = collection(db, "users", userId, "items");
+  const snapshot = await getDocs(itemsRef);
+
+  const items = [];
+  snapshot.forEach((doc) => {
+    items.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
+
+  return items;
+}
